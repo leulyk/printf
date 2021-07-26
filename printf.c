00cedfdc2ad5499e1fa6921dec4d3362623515ec
Shelter Mashonganyika <gigashelter@gmail.com>
2021-07-26 14:07:41 -0700

    n

--- printf.c
+++ printf.c
@@ -1,47 +1,38 @@
+#include <stdarg.h>
+#include <unistd.h>
 #include "holberton.h"
-
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+ * _printf - Prints formated text.
+ * @format: String with format of parameters.
+ * Return: Length of text printed.
  */
 int _printf(const char *format, ...)
 {
-int (*pfunc)(va_list, flags_t *);
-const char *p;
-va_list arguments;
-flags_t flags = {0, 0, 0};
+  int i = 0, c = 0;
+  va_list list;
 
-register int count = 0;
-
-va_start(arguments, format);
-if (!format || (format[0] == '%' && !format[1]))
-return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2])
-return (-1);
-for (p = format; *p; p++)
-{
-if (*p == '%')
-{
-p++;
-if (*p == '%')
-{
-count += _putchar('%');
-continue;
-}
-while (get_flag(*p, &flags))
-p++;
-pfunc = get_print(*p);
-count += (pfunc)
-? pfunc(arguments, &flags)
-: _printf("%%%c", *p);
-} else
-count += _putchar(*p);
-}
-_putchar(-1);
-va_end(arguments);
-return (count);
+  if (format)
+    {
+      va_start(list, format);
+      while (format[i])
+	{
+	  if (format[i] == '%')
+	    {
+	      i++;
+	      if (!format[i])
+		{
+		  return (-1);
+		}
+	      if (conv(format, &i, &c, &list))
+		{
+		  i++;
+		  continue;
+		}
+	    }
+	  write(1, format + i++, 1);
+	  c++;
+	}
+      return (c);
+    }
+  return (-1);
 }
