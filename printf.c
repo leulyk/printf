39c21df3b13761f37491963652b51974447a4e7b
Leul Yiheyis <leulykpro@gmail.com>
2021-07-26 16:05:05 +0300

    add format specifiers

--- printf.c
+++ printf.c
@@ -69,6 +69,11 @@ int process_format(va_list list, char ch)
 		{ 's', print_string },
 		{ 'd', print_integer },
 		{ 'i', print_integer },
+		{ 'o', print_octal },
+		{ 'x', print_hex_lower },
+		{ 'X', print_hex_upper },
+		{ 'b', print_binary },
+		{ 'u', print_integer },
 		{ 0, NULL }
 	};
 
