786bfd0b9d327039a2d20befc9f91094365c6189
Shelter Mashonganyika <gigashelter@gmail.com>
2021-07-26 13:09:31 -0700

    fourth correction

--- holberton.h
+++ holberton.h
@@ -1,33 +1,18 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 #include <stdarg.h>
-
-/**
- * struct printer - print different data depending on a format specifier
- *
- * @ch: format specifier
- * @print: pointer to a print function
- *
- * Description: a structure to hold a character to specify what to print
- * and pointer to functions with printing capabilities to various types
- */
-typedef struct printer
-{
-	char ch;
-	int (*print)(va_list);
-} printer_t;
-
 int _printf(const char *format, ...);
+int _strlen(char *s);
 int _putchar(char c);
-int process_format(va_list list, char c);
-int print_string(va_list list);
-int print_string_all(va_list list);
-int print_integer(va_list list);
-unsigned int _pow(int base, int exponent);
-int convert(unsigned int num, int base, char ch);
-int print_octal(va_list list);
-int print_hex_lower(va_list list);
-int print_hex_upper(va_list list);
-int print_binary(va_list list);
+unsigned int print_number(int n, short sign);
+short conv(const char *format, int *i, int *c, va_list *list);
+int print_base(unsigned int number, short base, short upper);
+int print_rev(char *s);
+int rot13(char *cypher);
+int case_p(unsigned long int ptr);
+int spchr(char *s);
+int flags(const char *format, int *i, va_list *list);
+int flags2(const char *, int *, va_list *, int, int, int, int);
 
-#endif
+#endif /* HOLBERTON_H */
