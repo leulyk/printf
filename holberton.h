88b8abcd40f16c0ca535ef8fa257dffab81e6aa7
Shelter Mashonganyika <gigashelter@gmail.com>
2021-07-26 13:59:49 -0700

    m

--- holberton.h
+++ holberton.h
@@ -1,18 +1,76 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdlib.h>
 #include <stdarg.h>
-int _printf(const char *format, ...);
-int _strlen(char *s);
-int _putchar(char c);
-unsigned int print_number(int n, short sign);
-short conv(const char *format, int *i, int *c, va_list *list);
-int print_base(unsigned int number, short base, short upper);
-int print_rev(char *s);
-int rot13(char *cypher);
-int case_p(unsigned long int ptr);
-int spchr(char *s);
-int flags(const char *format, int *i, va_list *list);
-int flags2(const char *, int *, va_list *, int, int, int, int);
 
-#endif /* HOLBERTON_H */
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+  int plus;
+  int space;
+  int hash;
+} flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+  char c;
+  int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/* print_nums */
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+
+/* print_bases */
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* _printf */
+int _printf(const char *format, ...);
+
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *);
+
+/* get_flag */
+int get_flag(char s, flags_t *f);
+
+/* print_alpha */
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
+/* print_custom */
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+
+/* print_address */
+int print_address(va_list l, flags_t *f);
+
+/* print_percent */
+int print_percent(va_list l, flags_t *f);
+
+#endif
