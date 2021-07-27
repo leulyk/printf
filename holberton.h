dcf6f1ad2ec7decbf7ecc0e7761297e49e2032cc
Leul Yiheyis <leulykpro@gmail.com>
2021-07-27 09:46:55 +0300

    add function prototypes

--- holberton.h
+++ holberton.h
@@ -1,18 +1,34 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 #include <stdarg.h>
-int _printf(const char *format, ...);
-int _strlen(char *s);
-int _putchar(char c);
-unsigned int print_number(int n, short sign);
-short conv(const char *format, int *i, int *c, va_list *list);
-int print_base(unsigned int number, short base, short upper);
-int print_rev(char *s);
-int rot13(char *cypher);
-int case_p(unsigned long int ptr);
-int spchr(char *s);
-int flags(const char *format, int *i, va_list *list);
-int flags2(const char *, int *, va_list *, int, int, int, int);
 
-#endif /* HOLBERTON_H */
+/**
+ * struct printer - structure to a format specifier and a function pointer
+ *
+ * @ch: the format specifier
+ * @print: a pointer to a function that takes a varialbe argument list
+ * and returns an integer
+ *
+ */
+typedef struct printer
+{
+	char ch;
+	int (*print)(va_list list);
+} printer_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int process_format(va_list list, char ch);
+int print_integer(va_list list);
+unsigned int _pow(int base, int exponent);
+int print_unsigned(va_list list);
+int print_string(va_list list);
+int print_string_all(va_list list);
+int convert(unsigned int num, int base, char ch);
+int print_octal(va_list list);
+int print_hex_lower(va_list list);
+int print_hex_upper(va_list list);
+int print_binary(va_list list);
+
+#endif
