aa8dd344c558156d15cb74b642505330aac5e1db
Leul Yiheyis <leulykpro@gmail.com>
2021-07-27 14:27:40 +0300

    add function prototypes

--- holberton.h
+++ holberton.h
@@ -23,12 +23,16 @@ int process_format(va_list list, char ch);
 int print_integer(va_list list);
 unsigned int _pow(int base, int exponent);
 int print_unsigned(va_list list);
+int print_char(va_list list);
 int print_string(va_list list);
 int print_string_all(va_list list);
+int print_string_rev(va_list list);
+int _strlen(char *str);
 int convert(unsigned int num, int base, char ch);
 int print_octal(va_list list);
 int print_hex_lower(va_list list);
 int print_hex_upper(va_list list);
 int print_binary(va_list list);
+int print_rot13(va_list list);
 
 #endif
