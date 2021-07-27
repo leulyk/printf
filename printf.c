7214d664f3ba79fd3212ec594a65f7482e0c56ab
Leul Yiheyis <leulykpro@gmail.com>
2021-07-27 09:47:25 +0300

    modify the main _printf function

--- printf.c
+++ printf.c
@@ -1,38 +1,85 @@
-#include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
 #include "holberton.h"
+
 /**
- * _printf - Prints formated text.
- * @format: String with format of parameters.
- * Return: Length of text printed.
+ * _printf - print a formatted string
+ *
+ * @format: character string to print composed of 0 or more directives
+ *
+ * Description: write output to standard output stream
+ *
+ * Return: number of characters printed (excluding the null byte)
  */
 int _printf(const char *format, ...)
 {
-  int i = 0, c = 0;
-  va_list list;
+	int index, count;
+	va_list data;
+	char c;
 
-  if (format)
-    {
-      va_start(list, format);
-      while (format[i])
+	index = count = 0;
+	va_start(data, format);
+	for (; *format && *(format + index); index++)
 	{
-	  if (format[i] == '%')
-	    {
-	      i++;
-	      if (!format[i])
+		for (; *(format + index) && *(format + index) != '%'; index++, count++)
+			_putchar(*(format + index));
+		if (!format[index])
+			return (count);
+		else if (*(format + index) == '%')
 		{
-		  return (-1);
+			index++;
+			c = *(format + index);
+			if (c == 'c')
+				_putchar(va_arg(data, int));
+			else if (c == '%')
+				_putchar('%');
+			else
+				count += process_format(data, c);
 		}
-	      if (conv(format, &i, &c, &list))
-		{
-		  i++;
-		  continue;
-		}
-	    }
-	  write(1, format + i++, 1);
-	  c++;
+		if (!format[index])
+			return (count);
+		count++;
 	}
-      return (c);
-    }
-  return (-1);
+	va_end(data);
+	return (count);
+}
+
+
+/**
+ * process_format - process a format specifier
+ *
+ * @list: the variable argument list
+ * @ch: character to process
+ *
+ * Return: number of characters printed
+ */
+int process_format(va_list list, char ch)
+{
+	int j = 0;
+	int count = 0;
+	printer_t p[] = {
+		{ 's', print_string },
+		{ 'S', print_string_all },
+		{ 'd', print_integer },
+		{ 'i', print_integer },
+		{ 'o', print_octal },
+		{ 'x', print_hex_lower },
+		{ 'X', print_hex_upper },
+		{ 'b', print_binary },
+		{ 'u', print_unsigned },
+		{ 0, NULL }
+	};
+
+	while (p[j].ch)
+	{
+		if (p[j].ch == ch)
+		{
+			count += p[j].print(list);
+			break;
+		}
+		++j;
+	}
+	return (count);
 }
